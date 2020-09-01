1656e8088ec1a10fae57ab38d65f3eed7c5b7978
ThunderSly <brunex887@gmail.com>
2020-09-01 16:27:39 -0300

    Com metodos estaticos e mult e div na ordem errada

--- Parser.py
+++ Parser.py
@@ -1,23 +1,24 @@
 import Tokenizer as tkr
 
 class Parser:
-
-    def parseExpression(self):
+    tokens = None
+    @staticmethod
+    def parseExpression():
         result = 0
-        self.tokens.selectNext()
-        current = self.tokens.actual
+        Parser.tokens.selectNext()
+        current = Parser.tokens.actual
         #print(current.value)
 
         if current.type == "INT":
             result = current.value
-            self.tokens.selectNext()
-            current = self.tokens.actual
+            Parser.tokens.selectNext()
+            current = Parser.tokens.actual
             #print(current.value)
-            while (current.type == "PLUS" or current.type == "MINUS") and current.type != "EOF":
-
+            while (current.type == "PLUS" or current.type == "MINUS" or current.type == "TIMES" or current.type == "DIVIDED") and current.type != "EOF":
+ 
                 if current.type == "PLUS":
-                    self.tokens.selectNext()
-                    current = self.tokens.actual
+                    Parser.tokens.selectNext()
+                    current = Parser.tokens.actual
                     #print(current.value)
                     if current.type == "INT":
                         result += current.value
@@ -26,25 +27,46 @@ class Parser:
                         raise Exception("Soma sem Numero depois")
 
                 elif current.type == "MINUS":
-                    self.tokens.selectNext()
-                    current = self.tokens.actual
+                    Parser.tokens.selectNext()
+                    current = Parser.tokens.actual
                     #print(current.value)
                     if current.type == "INT":
                         result -= current.value
 
                     else:
                         raise Exception("Subtracao sem Numero depois")
+
+                elif current.type == "TIMES":
+                    Parser.tokens.selectNext()
+                    current = Parser.tokens.actual
+                    #print(current.value)
+                    if current.type == "INT":
+                        result = current.value * result
+
+                    else:
+                        raise Exception("Multiplicacao sem Numero depois")
+                
+                elif current.type == "DIVIDED":
+                    Parser.tokens.selectNext()
+                    current = Parser.tokens.actual
+                    #print(current.value)
+                    if current.type == "INT":
+                        result = result/current.value
+
+                    else:
+                        raise Exception("Divisao sem Numero depois")
                     
-                self.tokens.selectNext()
-                current = self.tokens.actual
+                Parser.tokens.selectNext()
+                current = Parser.tokens.actual
                 #print(current.value)
 
             return result
         else:
             raise Exception("Expressao nao comeca com numero")
-
-    def run(self, code):
-        self.tokens = tkr.Tokenizer(code)
-        result = self.parseExpression()
+    
+    @staticmethod
+    def run(code):
+        Parser.tokens= tkr.Tokenizer(code)
+        result = Parser.parseExpression()
         #print("=")
         print(result)
